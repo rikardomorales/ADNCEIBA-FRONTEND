6cb9ce4a236530d2439aae040829ea7b4f88455d
Ricardo Alejandro Morales Penilla - Ceiba Software <ricardo.morales@ceiba.com.co>
2021-02-23 10:36:41 -0500

    front

--- src/app/feature/pago/pago.module.ts
+++ src/app/feature/pago/pago.module.ts
@@ -1,21 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { PagoRoutingModule } from './pago-routing.module'; 
-import { ListarPagoComponent } from './components/listar-pago/listar-pago.component'; 
-import { PagoComponent } from './components/pago/pago.component';
-import { SharedModule } from '@shared/shared.module';
-import { PagoService } from './shared/service/pago.service';
-
+import { PagoRoutingModule } from "./pago-routing.module";
+import { ListarPagoComponent } from "./components/listar-pago/listar-pago.component";
+import { PagoComponent } from "./components/pago/pago.component";
+import { SharedModule } from "@shared/shared.module";
+import { PagoService } from "./shared/service/pago.service";
 
 @NgModule({
-  declarations: [ 
-    ListarPagoComponent,
-    PagoComponent
-  ],
-  imports: [
-    PagoRoutingModule,
-    SharedModule
-  ],
-  providers: [PagoService]
+  declarations: [ListarPagoComponent, PagoComponent],
+  imports: [PagoRoutingModule, SharedModule],
+  providers: [PagoService],
 })
-export class PagoModule { }
+export class PagoModule {}
