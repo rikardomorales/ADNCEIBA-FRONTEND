4383d78995df1e401be01f5008f20c2a5f7ef1f7
Ricardo Alejandro Morales Penilla - Ceiba Software <ricardo.morales@ceiba.com.co>
2021-02-24 07:32:44 -0500

    front

--- src/app/feature/pago/pago.module.ts
+++ src/app/feature/pago/pago.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { PagoRoutingModule } from './pago-routing.module';
 import { ListarPagoComponent } from './components/listar-pago/listar-pago.component';
 import { PagoComponent } from './components/pago/pago.component';
-import { SharedModule } from '@shared/shared.module';
 import { PagoService } from './shared/service/pago.service';
 
 @NgModule({
   declarations: [ListarPagoComponent, PagoComponent],
-  imports: [PagoRoutingModule, SharedModule],
+  imports: [PagoRoutingModule],
   providers: [PagoService],
 })
 export class PagoModule {}
